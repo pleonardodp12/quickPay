72c136cfa10cc2adfbabdc56f6adcf443ae5eaa0
Leonardo Dias <pauloleonardo_dp@outlook.com>
2020-06-13 16:08:36 -0300

    TabBar gradient payment

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
+
+import PayButton from './components/PayButton'; 
 
 
 import Homescreen from './screens/Home';
@@ -7,10 +10,51 @@ import Walletscreen from './screens/Wallet';
 import Payscreen from './screens/Pay';
 
 const Tab = createBottomTabNavigator();
+const icons = {
+    Home: {
+        lib: AntDesign,
+        name: 'home',
+    },
+    Wallet: {
+        lib: AntDesign,
+        name: 'creditcard',
+    },
+    Notifications: {
+        lib: Ionicons,
+        name: 'ios-notifications-outline',
+    },
+    Settings: {
+        lib: AntDesign,
+        name: 'setting',
+    },
+}
 
 export default function Navigation() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route, navigation }) => ({
+                tabBarIcon: ({ color,size, focused }) => {
+                    if (route.name === 'Pay') {
+                        return (
+                            <PayButton
+                                onPress={() => navigation.navigate('Pay')}    
+                                focused={focused}
+                            />
+                        );
+                    }
+                    const { lib: Icon, name} = icons[route.name];
+                    return <Icon name={name} size={size} color={color} />
+                },
+            })}
+            tabBarOptions={{
+                style: {
+                    backgroundColor: '#131418',
+                    borderTopColor: 'rgba(255, 255, 255, 0.2)',
+                },
+                activeTintColor: '#fff',
+                inactiveTintColor: '#92929c'
+            }}
+        >
             <Tab.Screen
                 name="Home"
                 component={Homescreen}
@@ -29,7 +73,23 @@ export default function Navigation() {
                 name="Pay"
                 component={Payscreen}
                 options={{
-                    title: "Pagamento"
+                    title: ''
+                }}
+            />
+
+            <Tab.Screen
+                name="Notifications"
+                component={Payscreen}
+                options={{
+                    title: "Notificações"
+                }}
+            />
+
+            <Tab.Screen
+                name="Settings"
+                component={Payscreen}
+                options={{
+                    title: "Ajustes"
                 }}
             />
         </Tab.Navigator>
