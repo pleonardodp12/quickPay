8b79c8a8fa929999b8dc18edb4104f745d3ac0b7
Leonardo Dias <pauloleonardo_dp@outlook.com>
2020-06-13 21:06:18 -0300

    Activies and Suggestions

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+
+import { Wrapper, Header, Balance, BalanceTitle, BalanceContainer, Container } from './styles';
+
+import Suggestions from '../../components/Suggestions';
+import Activities from '../../components/Activities';
 
 export default function Home() {
-    return <Text>Home</Text>
+		return (
+				<Wrapper>
+					<Container>
+						<Header>
+								<MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+
+								<BalanceContainer>
+									<BalanceTitle>Meu Saldo</BalanceTitle>
+									<Balance>R$ 0,00</Balance>
+								</BalanceContainer>
+
+								<AntDesign name="gift" size={30} color="#10c86e" />
+
+						</Header>
+						<Suggestions />
+						<Activities />
+
+					</Container>
+				</Wrapper>		
+		);
 };
